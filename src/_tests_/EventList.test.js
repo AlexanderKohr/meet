18844892cbe1023ef90d018638141f6bd8e8c853
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-03-23 12:36:21 +0100

    Create tests and code for required functionality

---
+++ src/_tests_/EventList.test.js
@@ -0,0 +1,14 @@
+import EventList from '../EventList';
+import Event from '../Event';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import { mockData } from '../mock-data';
+
+describe ('<EventList /> component', () => {
+    test('render correct number of events', () => {
+        const EventListWrapper = shallow(<EventList events={mockData} />);
+        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+    });
+    
+});
