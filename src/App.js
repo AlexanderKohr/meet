5eef983bf955583676469a598c8972aff3498039
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-04-04 18:21:59 +0200

    Add WelcomeScreen to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import './nprogress.css';
 import './App.css';
 import React, { Component } from 'react';
-import { extractLocations, getEvents } from './api';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import CitySearch from './CitySearch';
 import EventList from './EventList';
 import NumberOfEvents from './NumberOfEvents';
+import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    showWelcomeScreen: undefined
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-      this.setState({ events, locations: extractLocations(events) });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
     }
-    });
+
+    if (!navigator.onLine) {
+      this.setState({
+        offlineText:
+        'Your are currently offline. The displayed events might not be up to date.'
+      });
+    } else {
+      this.setState({
+        offlineText: ''
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -62,12 +83,17 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className='App' />
+
     return (
       <div className='App'>
+        <OfflineAlert text={this.state.offlineText} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} 
             updateNumberOfEvents={this.updateNumberOfEvents}/>
         <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents}/>
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={()=> { getAccessToken() }} />
       </div>
     );
   }
