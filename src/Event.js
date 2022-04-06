79dc6e21de1d5cd7a4bf4db4532da8db11634d65
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-04-06 19:37:34 +0200

    Change to bootstrap layout

--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, Button, CardGroup } from "react-bootstrap";
 
 class Event extends Component {
     state = {
@@ -17,7 +18,42 @@ class Event extends Component {
         const { event } = this.props;
         const { collapsed } = this.state;
         return (
-        <div className="event">
+         <div className='event'>
+           <CardGroup>
+                <Card className='event-card'>
+                  <Card.Body>
+                    <Card.Title className="summary">{event.summary}</Card.Title>
+                      <Card.Subtitle className="start-date">{event.start.dateTime} ({event.start.timeZone})</Card.Subtitle>
+                      <Card.Subtitle className="location">@{event.summary} | {event.location}</Card.Subtitle>
+                            
+                        <Button 
+                          variant="outline-info"
+                          className={`details-button ${collapsed ? "show" : "hide"}-details`}
+                          onClick={this.handleClick}
+                        >
+                          {collapsed ? "Show Details" : "Hide Details"}
+                        </Button>
+
+                        {!collapsed && (
+                        <div
+                          className={`extra-details ${
+                          this.state.collapsed ? "hide" : "show"
+                          }`}
+                        >
+                          <Card.Title>About the event:</Card.Title>
+                          <a href={event.htmlLink} rel="noreferrer" target="_blank">
+                          See details on Google Calendar
+                          </a>
+                          <p className="event-description">{event.description}</p>
+                        </div>
+                        )}
+                            
+                  </Card.Body>
+                </Card>
+                </CardGroup>
+                </div>
+
+       /* <div className="event">
         <h2 className="summary">{event.summary}</h2>
         <p className="start-date">
           {event.start.dateTime} ({event.start.timeZone})
@@ -48,7 +84,7 @@ class Event extends Component {
             <p className="event-description">{event.description}</p>
           </div>
         )}
-      </div>
+      </div> */
 
         /* return <div className='event'>
             <button className='show-details'>Show details</button>
