5bb42b39fa2d35afb4760e7d6179ef5a93d73007
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-04-06 19:38:23 +0200

    Change layout

--- src/EventList.js
+++ src/EventList.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
 import Event from './Event';
 
 class EventList extends Component {
     render() {
         const { events } = this.props;
         return (
-            <ul className='EventList'>
+            <Row className='EventList'>
                 {events.map((event) => (
-                    <li key={event.id}>
+                    <Col key={event.id}>
                         <Event event={event} />
-                    </li>))}
-            </ul>
+                    </Col>
+                    ))}
+            </Row>
         );
     }
 }
